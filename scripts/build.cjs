98af1ee5dcf6e3b38d03822c0fef97a22dfc5ed4
Jake Strawn <jake.strawn@gmail.com>
2022-09-24 10:17:36 -0400

    fix(init): Enabling FULL TURBO for inilne-tokens.

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -2,8 +2,6 @@ const { series } = require("async");
 const { exec } = require("child_process");
 
 series([
-  // Remove any prior build assets.
-  (callback) => exec("rm -rf ./dist", callback),
   // Build the client-side assets.
   (callback) =>
     exec(
