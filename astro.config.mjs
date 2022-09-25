359f61c0b221bf87b38b61291453f6c8a07cb6df
Jake Strawn <jake.strawn@gmail.com>
2022-09-25 09:06:40 -0400

    feat(init): Implementing documentation templates, mdx format and more.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,24 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
+import react from "@astrojs/react";
 import lit from "@astrojs/lit";
-
+import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [lit()]
+  markdown: {
+    shikiConfig: {
+      theme: "dracula",
+    },
+  },
+  integrations: [
+    // Enable support for MDX.
+    mdx(),
+    // Enable Preact to support Preact JSX components.
+    preact(),
+    // Enable React for the Algolia search component.
+    react(),
+    // Enable Lit for Web Component integration.
+    lit(),
+  ],
+  site: `http://localhost:3000/`,
 });
---
+++ src/config.ts
@@ -0,0 +1,58 @@
+export const SITE = {
+  title: "InlineJS",
+  description: "InlineJS Design System.",
+  defaultLanguage: "en_US",
+};
+
+export const OPEN_GRAPH = {
+  image: {
+    src: "https://github.com/withastro/astro/blob/main/assets/social/banner.jpg?raw=true",
+    alt:
+      "astro logo on a starry expanse of space," +
+      " with a purple saturn-like planet floating in the right foreground",
+  },
+  twitter: "himerus",
+};
+
+// This is the type of the frontmatter you put in the docs markdown files.
+export type Frontmatter = {
+  title: string;
+  description: string;
+  layout: string;
+  image?: { src: string; alt: string };
+  dir?: "ltr" | "rtl";
+  ogLocale?: string;
+  lang?: string;
+};
+
+export const KNOWN_LANGUAGES = {
+  English: "en",
+} as const;
+export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
+
+export const GITHUB_EDIT_URL = `https://github.com/inlinejs/inline`;
+
+export const COMMUNITY_INVITE_URL = `https://github.com/inlinejs/inline`;
+
+// See "Algolia" section of the README for more information.
+export const ALGOLIA = {
+  indexName: "inlinejs_documentation",
+  appId: "GT1T4MY87D",
+  apiKey: "8022fc943afa4d88a3e0dcc9288e2060",
+};
+
+export type Sidebar = Record<
+  typeof KNOWN_LANGUAGE_CODES[number],
+  Record<string, { text: string; link: string }[]>
+>;
+export const SIDEBAR: Sidebar = {
+  en: {
+    Documentation: [
+      { text: "Introduction", link: "en/introduction" },
+      { text: "Page 2", link: "en/page-2" },
+      { text: "Page 3", link: "en/page-3" },
+    ],
+    "Design Tokens": [{ text: "Page 4", link: "en/page-4" }],
+    "Component Library": [{ text: "Page 5", link: "en/page-5" }],
+  },
+};
