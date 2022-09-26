9f7529079539d0e6ad6ec36abd07c669a3cbd733
Jake Strawn <jake.strawn@gmail.com>
2022-09-25 22:12:20 -0400

    fix(init): Update to correct edit URL pattern.

--- src/config.ts
+++ src/config.ts
@@ -32,7 +32,7 @@ export const KNOWN_LANGUAGES = {
 } as const;
 export const KNOWN_LANGUAGE_CODES = Object.values(KNOWN_LANGUAGES);
 
-export const GITHUB_EDIT_URL = `https://github.com/inlinejs/inline`;
+export const GITHUB_EDIT_URL = `https://github.com/inlinejs/inline/blob/main`;
 
 export const COMMUNITY_INVITE_URL = `https://discord.gg/U9hr82Cr`;
 
